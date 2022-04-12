30b561703fcc6d61b1903215cdfc4ab609cc5d95
s26river <angra111@mti.biglobe.ne.jp>
2022-04-12 13:29:54 +0900

    Ver1.7

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from datetime import datetime,date,timedelta,timezone
 def graph_plt():
     
     
-    TITLE=日本のコロナ感染者数推移
+    TITLE='日本のコロナ感染者数推移'
 
-    st.title(f"{TITLE}")
+    st.title(f'{TITLE}')
 
     #status_area = st.empty()
     # カウントダウン
