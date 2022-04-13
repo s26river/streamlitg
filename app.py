f0924e9e5b824ccf39ccc2e7520df57e8600d41d
s26river <51350226+s26river@users.noreply.github.com>
2022-04-13 13:38:33 +0900

    PANDAS

--- app.py
+++ app.py
@@ -26,8 +26,11 @@ def sake():
     "銘柄ごとフレーバータグ": "https://muro.sakenowa.com/sakenowa-data/api/brand-flavor-tags",
     }
         # 地域名を取得
-    areas_response = requests.get(urls.get("地域一覧")).json()
-    areas = [area["name"] for area in areas_response["areas"]]
+    #areas_response = requests.get(urls.get("地域一覧")).json()
+    #areas = [area["name"] for area in areas_response["areas"]]
+    #リスト内包表記をPANDASに変更
+    areas_response = requests.get("https://muro.sakenowa.com/sakenowa-data/api/areas").json()['areas']
+    areas=pd.DataFrame(areas_response)['name']       
     select_areas = st.sidebar.selectbox("好きな地域を選んでください", areas)
     # 地域IDを取得
     areaId = [area["id"] for area in areas_response["areas"] if area["name"]==select_areas][0]
