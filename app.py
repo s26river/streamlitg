b8e5744a7d8d4aaf23c8e8352845680674791086
s26river <51350226+s26river@users.noreply.github.com>
2022-04-13 12:08:11 +0900

    SAKE0.1

--- app.py
+++ app.py
@@ -3,51 +3,63 @@ import pandas as pd
 import numpy as np
 import time
 from datetime import datetime,date,timedelta,timezone
-"""
-# 日本のコロナ感染者数推移
-"""
+import pandas as pd
+import numpy as np
+import requests
+import json
+import plotly.express as px
 
-def graph_plt(): 
+"""
+# 簡単日本酒選定アプリ
+## [さけのわAPI](https://sakenowa.com)のデータを表示しています 
+"""
+def sake(): 
     
-    #ILE='日本のコロナ感染者数推移'
-    #.title(f'{TITLE}')
-   
-    UNOW=datetime.now()
-    JST=timezone(timedelta(hours=+9))
-    NOW=UNOW+timedelta(hours=+9)
-    TIME=datetime(NOW.year,NOW.month,NOW.day,NOW.hour,NOW.minute,tzinfo=JST)
-    STIME=TIME.strftime("%Y年%m月%d日%H時%M分")
-    st.write(f"感染者数推移{STIME}現在")
-    df = pd.read_csv('https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv',parse_dates=True,index_col='Date')
-    dfp=df
-    dfp.columns.name="感染者数"
-    dfp.index.name="日付"
-    dfpc=dfp.rename(columns={'ALL':'全国','Aichi':'愛知県','Akita':'秋田県','Aomori':'青森県'\
-,'Chiba':'千葉県','Ehime':'愛媛県','Fukui':'福井県','Fukuoka':'福岡県','Fukushima':'福島県'\
-,'Gifu':'岐阜県','Gunma':'群馬県','Hiroshima':'広島県','Hokkaido':'北海道','Hyogo':'兵庫県'\
-,'Ibaraki':'茨城県','Ishikawa':'石川県','Iwate':'岩手県','Kagawa':'香川県','Kagoshima':'鹿島県'\
-,'Kanagawa':'神奈川県','Kochi':'高知県','Kumamoto':'熊本県','Kyoto':'京都府','Mie':'三重県','Miyagi':'宮城県'\
-,'Miyazaki':'宮崎県','Nagano':'長野県','Nagasaki':'長崎県','Nara':'奈良県','Niigata':'新潟県','Oita':'大分県'\
-,'Okayama':'岡山県','Okinawa':'沖縄県','Osaka':'大阪府','Saga':'佐賀県','Saitama':'埼玉県','Shiga':'滋賀県'\
-,'Shimane':'島根県','Shizuoka':'静岡県','Tochigi':'栃木県','Tokushima':'徳島県','Tokyo':'東京都','Tottori':'鳥取県'\
-,'Toyama':'富山県','Wakayama':'和歌山県','Yamagata':'山形県','Yamaguchi':'山口県','Yamanashi':'山梨県'})
-    TITLE="コロナ新規陽性者数（県別）移動平均28日"
-    dfpcd=dfpc[dfpc.index >= '2020-07-07']
-    dr=dfpcd.resample("7D").mean()
-    dr07=dfpcd.rolling(window=7).mean()#移動平均7日
-    dr28=dfpcd.rolling(window=28).mean()#移動平均28日
-    chart_data = pd.DataFrame({"全国移動平均7日":dr07["全国"],"全国移動平均28日":dr28["全国"],
-                               "東京都移動平均7日":dr07["東京都"],"東京都移動平均28日":dr28["東京都"],
-                               "神奈川移動平均7日":dr07["神奈川県"],"神奈川移動平均28日":dr28["神奈川県"]})
+    # エンドポイント
+    urls = {
+    "地域一覧": "https://muro.sakenowa.com/sakenowa-data/api/areas",
+    "銘柄一覧": "https://muro.sakenowa.com/sakenowa-data/api/brands",
+    "蔵元一覧": "https://muro.sakenowa.com/sakenowa-data/api/breweries",
+    "ランキング": "https://muro.sakenowa.com/sakenowa-data/api/rankings",
+    "フレーバーチャート": "https://muro.sakenowa.com/sakenowa-data/api/flavor-charts",
+    "フレーバータグ": "https://muro.sakenowa.com/sakenowa-data/api/flavor-tags",
+    "銘柄ごとフレーバータグ": "https://muro.sakenowa.com/sakenowa-data/api/brand-flavor-tags",
+    }
     
-    if st.checkbox('折線グラフの表示'):
-        st.line_chart(chart_data)
-    if st.checkbox('データの表示'):
-        st.dataframe(chart_data)
-    #st.table(chart_data)
-    st.text('Version 3.0')
-    # マークダウンテキスト
-    #st.markdown('**Markdown is available **')
-
+    # 地域名を取得
+    areas_response = requests.get(urls.get("地域一覧")).json()
+    areas = [area["name"] for area in areas_response["areas"]]
+    select_areas = st.sidebar.selectbox("好きな地域を選んでください", areas)
+    # 地域IDを取得
+    areaId = [area["id"] for area in areas_response["areas"] if area["name"]==select_areas][0]
+　　# 蔵元名を取得
+    breweries_response = requests.get(urls.get("蔵元一覧")).json()
+    breweries = [breweries["name"] for breweries in breweries_response["breweries"] if breweries["areaId"]==areaId]
+    select_breweries = st.sidebar.selectbox("好きな蔵元を選んでください", breweries)
+    # 蔵元IDを取得
+    breweryId = [breweries["id"] for breweries in breweries_response["breweries"] if breweries["name"]==select_breweries][0]
+    # 銘柄名を取得
+    brands_response = requests.get(urls.get("銘柄一覧")).json()
+    brands = [brands["name"] for brands in brands_response["brands"] if brands["breweryId"]==breweryId]
+    select_brands = st.sidebar.selectbox("好きな銘柄を選んでください", brands)
+    # 銘柄IDを取得
+    brandId = [brands["id"] for brands in brands_response["brands"] if brands["name"]==select_brands][0]
+    
+    # フレーバーチャートを取得
+    flavor_charts_response = requests.get(urls.get("フレーバーチャート")).json()
+    flavor_charts = [flavor_charts for flavor_charts in flavor_charts_response["flavorCharts"] if flavor_charts["brandId"]==brandId]
+    # plotlyでレーダーチャートを表示
+    st.markdown(f'## {select_brands}のフレーバーチャート')
+    try:
+        df = pd.DataFrame(flavor_charts)
+        df = df.drop('brandId', axis=1)
+        # 見やすくするためにカラム名を変更、その後plotlyで読み込めるようにデータを転置
+        df = df.rename(columns={'f1':'華やか', 'f2':'芳醇', 'f3':'重厚', 'f4':'穏やか', 'f5':'ドライ', 'f6':'軽快'}).T
+        fig = px.line_polar(df, r=df[0], theta=df.index, line_close=True, range_r=[0,1])
+        st.plotly_chart(fig)
+    # フレーバーチャートのデータがないものもあるので例外処理
+    except:
+        st.markdown('## この銘柄はフレーバーチャートを表示できません！！')
+        
 if __name__=='__main__':
-    graph_plt()
+    sake()
