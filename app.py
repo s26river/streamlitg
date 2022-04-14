9c363a2a492ba0a2536839f9df001b1f614f0ee2
s26river <51350226+s26river@users.noreply.github.com>
2022-04-14 15:53:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -84,16 +84,15 @@ def sake():
     if st.checkbox('ギャラリーの表示'):
         stc.html(
             """
-        <!DOCTYPE html>
-        <html>
-        <head><!-- 裏設定エリアの開始 -->
-        <meta charset="utf-8"><!-- 文字化け防止 -->
-        </head><!-- 裏設定エリアの終了 -->
-        <body><!-- 画面に表示されるエリアの開始 -->
+            <!DOCTYPE html>
+            <html>
+            <head><!-- 裏設定エリアの開始 -->
+            <meta charset="utf-8"><!-- 文字化け防止 -->
+            </head><!-- 裏設定エリアの終了 -->
+            <body><!-- 画面に表示されるエリアの開始 -->
             わたしはもぐたんです。
-        </body><!-- 画面に表示されるエリアの終了 -->
-        </html>
-　　　　"""
-        #stc.iframe("STREAMLITG.html")        
+            </body><!-- 画面に表示されるエリアの終了 -->
+            </html>
+            """     
 if __name__=='__main__':
     sake()
