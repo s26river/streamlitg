5112e61b87ebe9a4ab01d67156137f709c81c359
s26river <51350226+s26river@users.noreply.github.com>
2022-04-14 16:38:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,10 @@ st.write(f'<span style="color:maroon">日本酒ソムリエ</span>',unsafe_allow
 #image = Image.open('./sake.jpg',use_column_width=True)
 #st.image('sake.jpg')
 num=2
-lcol=[]
-col= st.beta_columns(num)
-for i in list(range(0,num,1)):
+col= st.columns(num)
+for i in range(1,num+1)):
     with col[i]:
-        #st.header("酒"+str(i+1))
+        st.header("酒"+str(i+1))
         #st.image("sake" + "str(i+1)" + ".jpg", use_column_width=True)
         st.image("sake.jpg", use_column_width=True)
 
