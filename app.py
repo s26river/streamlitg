160d77dd510c1895f7644c3b532e00e0b85f6c66
s26river <51350226+s26river@users.noreply.github.com>
2022-04-15 14:21:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def sake():
     areas = df_areas_response['name'].values  #地域名一覧
     select_areas = select_areas = st.sidebar.selectbox("好きな地域を選んでください", areas)
     areaId = get_areaId(select_areas) #地域IDを取得    
-    breweries_response = get_breweries_response() #蔵元名を取得
+    breweries_response = get_breweries_response() #蔵元名一覧を取得
     breweries = [breweries["name"] for breweries in breweries_response["breweries"] if breweries["areaId"]==areaId]
     select_breweries = st.sidebar.selectbox("好きな蔵元を選んでください", breweries)
     # 蔵元IDを取得
