21c880f9acb6bfe7e9ea4985afd46306731bd29a
satoshi <angra111@mti.biglobe.ne.jp>
2022-04-17 23:07:34 +0900

    REFACT2

--- app.py
+++ app.py
@@ -10,15 +10,10 @@ import requests
 import json
 import plotly.express as px
 
-#st.write(f'<span style="color:maroon;font-size:xx-large;font-weight:bolder">日本酒AIソムリエ</span>',unsafe_allow_html=True)
 st.write(f'<span style="font-size:xx-large;font-weight:bolder">日本酒AIソムリエ</span>',unsafe_allow_html=True)
-st.write(f'<span>[さけのわ様のAPI](https://sakenowa.com)を使用しています</span>',unsafe_allow_html=True)
-
 col1,col2,col3= st.columns(3)
 col1.image("image/sake1.jpg", use_column_width=True)
-#col1.image("image/sake_tsugu.gif", use_column_width=True)
-#col2.image("sake2.jpg", use_column_width=True)
-#col3.image("sake3.jpg", use_column_width=True)
+st.write(f'<span style="font-size:small">[さけのわ様のAPI](https://sakenowa.com)を使用しています</span>',unsafe_allow_html=True)
 
 #エンドポイント
 urls = {
