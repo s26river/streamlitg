6c58030d0cec8c2fbb6ba69afc710efea527c9ce
s26river <51350226+s26river@users.noreply.github.com>
2022-04-18 18:49:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -70,13 +70,12 @@ def sake():
         df=df.drop('brandId', axis=1)
         #見やすくするためにカラム名を変更、その後plotlyで読み込めるようにデータを転置
         df=df.rename(columns={'f1':'華やか', 'f2':'芳醇', 'f3':'重厚', 'f4':'穏やか', 'f5':'ドライ', 'f6':'軽快'}).T
-        df
+        df.plot.bar()
         #fig = px.line_polar(df, r=df[0], theta=df.index, line_close=True, range_r=[0,1],width=350,height=350)
         #left_column,mid,right_column = st.columns(3)
         #left_column.plotly_chart(fig)
       else:
-        df
-        st.write(f'<span style="color:red;background:pink">この銘柄はフレーバーチャートを表示できません！！</span>',unsafe_allow_html=True)
+        st.write(f'<span style="color:red;background:pink">この銘柄は味の分析が出来ません！！</span>',unsafe_allow_html=True)
         
 if __name__=='__main__':
       sake()
