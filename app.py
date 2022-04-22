b5e4a3d25da2a6cad9346e83795a15127419a5b4
s26river <51350226+s26river@users.noreply.github.com>
2022-04-22 10:46:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -83,13 +83,14 @@ def sake():
     except:
       st.write(f'<span style="color:red;background:pink">この銘柄はフレーバーチャートを表示できません！！</span>',unsafe_allow_html=True)
     
-    try:
-      #ランキングデータフレーム作成
-      brandId=brandId.values[0]
-      ranking=get_rank('ランキング','overall',brandId)   
-      st.write(f'<span style="font-size:medium">「{text}」の全国ランキングは{ranking}位です。</span>',unsafe_allow_html=True)
-    except:
-      st.write(f'<span style="color:red;background:pink">この銘柄はランキングを表示できません！！</span>',unsafe_allow_html=True)
+    if st.checkbox('全国ランキングの表示')
+      try:
+      　#ランキングデータフレーム作成
+      　brandId=brandId.values[0]
+      　ranking=get_rank('ランキング','overall',brandId)   
+      　st.write(f'<span style="font-size:medium">「{text}」の全国ランキングは{ranking}位です。</span>',unsafe_allow_html=True)
+    　except:
+      　st.write(f'<span style="color:red;background:pink">この銘柄はランキングを表示できません！！</span>',unsafe_allow_html=True)
   
 if __name__=='__main__':
       sake()
