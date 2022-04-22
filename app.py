bdc3e859abd476d5b1b2e798ee38d3ba5267ddef
s26river <51350226+s26river@users.noreply.github.com>
2022-04-22 15:46:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def sake():
     try:
       #ランキングデータフレーム
       brandId=brandId.values[0]
-      ranking=get_rank('ランキング','areas',brandId)
+      ranking=get_rank('ランキング','area',brandId)
       st.write(f'<span style="font-size:medium">「{text}」の同県内でのランキングは{ranking}位です。</span>',unsafe_allow_html=True)
     except:
       st.write(f'<span style="color:red;background:pink">この銘柄は県内ランキングを表示できません！！</span>',unsafe_allow_html=True)
