63e1cfee4bdf8f40fa22d223089140635dec7111
s26river <51350226+s26river@users.noreply.github.com>
2022-04-22 10:46:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def sake():
     except:
       st.write(f'<span style="color:red;background:pink">この銘柄はフレーバーチャートを表示できません！！</span>',unsafe_allow_html=True)
     
-    if st.checkbox('全国ランキングの表示')
+    if st.checkbox('全国ランキングの表示'):
       try:
       　#ランキングデータフレーム作成
       　brandId=brandId.values[0]
