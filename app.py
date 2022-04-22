ca3bbf2f3595d11babbb1791271f5f35641dae7e
s26river <51350226+s26river@users.noreply.github.com>
2022-04-22 10:05:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def sake():
       
     #ランキングデータフレーム作成
     rank_response = requests.get(urls.get("ランキング")).json()
-    df_rank=pd.DataFrame(rank_response['overall'])     
+    df_rank=pd.DataFrame(rank_response['overall']).set_index('rank')     
     df_rank
    
     ##st.write(f'<span style="font-size:small">全国ランキング{rank}位</span>',unsafe_allow_html=True)
